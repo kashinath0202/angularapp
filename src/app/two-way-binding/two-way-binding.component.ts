a804db5c01198135c89e72b242c0822ccbd37584
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-11-24 22:26:58 +0530

    Two Way Binding
    Two Way Binding

---
+++ src/app/two-way-binding/two-way-binding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-two-way-binding',
+  templateUrl: './two-way-binding.component.html',
+  styleUrls: ['./two-way-binding.component.css']
+})
+export class TwoWayBindingComponent  {
+
+value:any='Angular Developer'
+
+  constructor() { }
+
+
+}
