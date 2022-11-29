8e1a24f4d80640241a1098a3d11c13bb383f5b0b
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-11-29 16:25:39 +0530

    Binding Project
    Binding Project

---
+++ src/app/binding-practice/binding-practice.component.ts
@@ -0,0 +1,41 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-binding-practice',
+  templateUrl: './binding-practice.component.html',
+  styleUrls: ['./binding-practice.component.css']
+})
+export class BindingPracticeComponent {
+
+  constructor() { }
+
+  text:string='';
+
+  eventHandling(event){
+  let value = event.target.value;
+
+  this.text=value;
+  
+  }
+
+  textSize:number=10;
+
+  setsize(operation){
+    if(operation=='incr'){
+      this.textSize += 1;
+    }else{
+      this.textSize -= 1;
+    }
+  }
+
+  isColor='';
+
+  setColor(event){
+
+    let val = event.target.value;
+  this.isColor=val;
+
+  }
+
+  
+}
