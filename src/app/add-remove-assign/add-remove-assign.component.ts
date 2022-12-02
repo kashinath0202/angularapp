d15f3d277254c9d4870293ad7c06bc89b64336fd
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-12-02 22:58:17 +0530

    Add & Remove data
    Add & Remove data

---
+++ src/app/add-remove-assign/add-remove-assign.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-remove-assign',
+  templateUrl: './add-remove-assign.component.html',
+  styleUrls: ['./add-remove-assign.component.css']
+})
+export class AddRemoveAssignComponent implements OnInit {
+
+  constructor() { }
+
+  mobileName:string;
+
+  saveData(value){
+     
+  }
+
+  data:any[]=[]
+
+  addData(values){
+      this.data.push({mobileName:values})
+  }
+
+
+  removeMobile(value){
+   this.data.splice(value,1)
+  }
+
+  ngOnInit() { 
+  }
+
+}
