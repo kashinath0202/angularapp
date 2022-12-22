c5ed06f1a98b07b25de2bc5a224bdcd136c7339b
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-12-22 21:13:08 +0530

    Subject & Behavior Subject using Subscribe
    Subject & Behavior Subject using Subscribe

---
+++ src/app/models/myEmployee.ts
@@ -0,0 +1,16 @@
+export class Employee{
+FirstName:string
+MiddleName:string
+LastName:string
+DateOfBirth:number
+age:number
+Username:any
+EmailID:any 
+MobileNumber:number
+Gender:string
+DeveloperRoll:string
+District:string
+PermanentAddress:any
+City:any
+PinCode:number
+}
