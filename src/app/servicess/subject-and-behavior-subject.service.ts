35b3ead5e870ad3950af9efb56fec613451939ab
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-12-23 19:25:16 +0530

    Update
    Update

--- src/app/servicess/subject-and-behavior-subject.service.ts
+++ src/app/servicess/subject-and-behavior-subject.service.ts
@@ -9,7 +9,7 @@ export class SubjectAndBehaviorSubjectService {
 
   // userName = new Subject<any>();    // create one property  observable (subject)  & we can do not set of default value
 
-  userName = new BehaviorSubject(' Angular Developer');    // we can  set of default value.
+  userName = new BehaviorSubject('I am  Angular Developer');    // we can  set of default value.
 
   constructor() { }
 }
