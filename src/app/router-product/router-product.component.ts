5a07436510d658b16d8802c81e0439bcc7e71ed3
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-12-25 22:30:18 +0530

    Router Product Page
     Router Product Page

---
+++ src/app/router-product/router-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-router-product',
+  templateUrl: './router-product.component.html',
+  styleUrls: ['./router-product.component.css']
+})
+export class RouterProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
