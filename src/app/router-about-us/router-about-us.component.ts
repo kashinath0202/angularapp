e6b638fa3a9466680a33a2194836d199f5fbd2ed
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-12-25 22:26:33 +0530

    Router AboutUs Page
    Router AboutUs Page

---
+++ src/app/router-about-us/router-about-us.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-router-about-us',
+  templateUrl: './router-about-us.component.html',
+  styleUrls: ['./router-about-us.component.css']
+})
+export class RouterAboutUsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
