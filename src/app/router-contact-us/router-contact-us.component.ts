d7d6cee0485ed85dc4183073d72e02ddc4232c65
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-12-28 17:40:54 +0530

    New tab ContactUs & use by the router
    New tab ContactUs & use by the router

---
+++ src/app/router-contact-us/router-contact-us.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-router-contact-us',
+  templateUrl: './router-contact-us.component.html',
+  styleUrls: ['./router-contact-us.component.css']
+})
+export class RouterContactUsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
