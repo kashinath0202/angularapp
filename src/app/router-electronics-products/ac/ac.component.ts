4319080a310fbfcaf225a2df4c3d151623c7d50e
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-12-28 17:31:39 +0530

    AC component with child electricalProduct
    AC component with child electricalProduct

---
+++ src/app/router-electronics-products/ac/ac.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ac',
+  templateUrl: './ac.component.html',
+  styleUrls: ['./ac.component.css']
+})
+export class AcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
