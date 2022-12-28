eaf209981c1015f1bce33bda8005c6e20f912566
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-12-28 17:41:12 +0530

    New tab Home & use by the router
    New tab Home & use by the router

---
+++ src/app/router-home/router-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-router-home',
+  templateUrl: './router-home.component.html',
+  styleUrls: ['./router-home.component.css']
+})
+export class RouterHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
