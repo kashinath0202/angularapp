fe8be3bf9db21731d1a6494221dfd406d0842a5a
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-12-28 17:32:24 +0530

    Ref. component with child electricalProduct
    Ref. component with child electricalProduct

---
+++ src/app/router-electronics-products/refrigerators/refrigerators.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-refrigerators',
+  templateUrl: './refrigerators.component.html',
+  styleUrls: ['./refrigerators.component.css']
+})
+export class RefrigeratorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
