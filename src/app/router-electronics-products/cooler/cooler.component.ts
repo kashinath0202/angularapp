ba85ab35d1f2b43941c73e3d12fdcaa0d27b0714
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-12-28 17:32:02 +0530

    Cooler component with child electricalProduct
    Cooler component with child electricalProduct

---
+++ src/app/router-electronics-products/cooler/cooler.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cooler',
+  templateUrl: './cooler.component.html',
+  styleUrls: ['./cooler.component.css']
+})
+export class CoolerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
