e888dfec3d05c36261a8c00151ecaf145dd28384
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-12-30 21:26:16 +0530

    authGuard apply
    authGuard apply

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { RouteGuardFiveComponent } from './route-guard-five/route-guard-five.component';
+import { RouteGuardFourComponent } from './route-guard-four/route-guard-four.component';
+import { RouteGuardOneComponent } from './route-guard-one/route-guard-one.component';
+import { RouteGuardThreeComponent } from './route-guard-three/route-guard-three.component';
+import { ActiveSecondGuard } from './routeGuard/active-second.guard';
+import { DeactivateGuard } from './routeGuard/deactivate.guard';
 
 import { RouterAboutUsComponent } from './router-about-us/router-about-us.component';
 import { RouterContactUsComponent } from './router-contact-us/router-contact-us.component';
@@ -13,17 +19,18 @@ import { FortunerComponent } from './router-product/fortuner/fortuner.component'
 import { RangeRoverComponent } from './router-product/range-rover/range-rover.component';
 import { RollsRoyceComponent } from './router-product/rolls-royce/rolls-royce.component';
 import { RouterProductComponent } from './router-product/router-product.component';
+import { RouterRegistrationFormComponent } from './router-registration-form/router-registration-form.component';
 import { ServiceModule } from './routerLoading/service.module';
 import { ServiceComponent } from './routerLoading/service/service.component';
 import { CustomPreloadingService } from './servicess/custom-preloading.service';
 
-
+ 
 const routes: Routes = [
   {path:'', redirectTo:'logIn', pathMatch:'full'},        // it should be first component
   {path:'logIn', component:RouterLogInComponent},
-  {path:'home',data:{preload:true}, loadChildren:'./router-home/home.module#HomeModule'},          // Using Lazy Loading & custom Preloading (data:{preloading:true})
-  {path:'aboutUs',loadChildren:'./router-about-us/about.module#AboutModule'},  // Using Lazy Loading
-  {path:'contactUs', component:RouterContactUsComponent},
+  {path:'home',data:{preload:true}, loadChildren:'./router-home/home.module#HomeModule'},          // Using Lazy Loading & custom Preloading (data:{preloading:true}) 
+  {path:'aboutUs',loadChildren:'./router-about-us/about.module#AboutModule'},  // Using Lazy Loading && & canActiveGuard
+  {path:'contactUs', component:RouterContactUsComponent}, 
   // {path:'cars',                // Move to Feature Module                                  //localhost:4200/cars
   //               children:[
   //                 {path:'',component:RouterProductComponent},           //localhost:4200/cars
@@ -37,11 +44,18 @@ const routes: Routes = [
   {path:'cars',data:{preload:true},loadChildren:'./router-product/product.module#ProductModule'},  // Using Lazy Loading & custom Preloading (data:{preloading:true})
   {path:'posts',component:RouterParameterizeDemoComponent},             //localhost:4200/posts    
   {path:'postDetails/:id',component:RouterParaPostDetailsComponent},
-  {path:'service',loadChildren:'./routerLoading/service.module#ServiceModule'}, // Using Lazy Loading
-   {path:'**', component:RouterPageNotFoundComponent}                    //wild card route  It should be last component
+  {path:'service',loadChildren:'./routerLoading/service.module#ServiceModule'},          // Using Lazy Loading
+  {path:'canActive',component:RouteGuardOneComponent,canActivate:[ActiveSecondGuard]},            // canActive Route Guard              
+  {path:'registrationForm',component:RouterRegistrationFormComponent,canDeactivate:[DeactivateGuard]},
+  {path:'canActiveChild',component:RouteGuardThreeComponent}, 
+  {path:'resolve',component:RouteGuardFourComponent}, 
+  {path:'canLoad',component:RouteGuardFiveComponent}, 
+  
+  // {path:'**', component:RouterPageNotFoundComponent}                    //wild card route  It should be last component
 ];
 
-@NgModule({                          //,{preloadingStrategy:PreloadAllModules}  use PreLoading Syntax     
+@NgModule({
+                            //,{preloadingStrategy:PreloadAllModules}  use PreLoading Syntax     
   imports: [RouterModule.forRoot(routes,{preloadingStrategy:CustomPreloadingService}), // This is custom preloading using Service(CustomPreloadingService)
             //  AboutModule,
             //  ServiceModule   // Because of use lazy loading
