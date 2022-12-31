1fb37edf5bc97be62f360c6be0b3120b9ddbfa9c
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2023-01-01 04:59:43 +0530

    AuthGaurd loaded
    AuthGaurd loaded

---
+++ src/app/route-guard-five/route-guard-five.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-route-guard-five',
+  templateUrl: './route-guard-five.component.html',
+  styleUrls: ['./route-guard-five.component.css']
+})
+export class RouteGuardFiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
