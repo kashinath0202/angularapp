cb0bd1aba9ec13a7491e89169d7d99d0963915d7
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2023-01-01 04:58:44 +0530

    AuthGaurd Resolve
    AuthGaurd Resolve

---
+++ src/app/route-guard-four/route-guard-four.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-route-guard-four',
+  templateUrl: './route-guard-four.component.html',
+  styleUrls: ['./route-guard-four.component.css']
+})
+export class RouteGuardFourComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
