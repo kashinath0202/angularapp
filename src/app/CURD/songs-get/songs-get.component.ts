51746585c472e8df539c469683a6be79c0d706a8
kashi <kashinathjeure@gmail.com>
2023-01-01 22:11:43 +0530

    Get Method (API)
    Get Method (API)

---
+++ src/app/CURD/songs-get/songs-get.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { SongsGetService } from 'src/app/services/songs-get.service';
+
+@Component({
+  selector: 'app-songs-get',
+  templateUrl: './songs-get.component.html',
+  styleUrls: ['./songs-get.component.css']
+})
+export class SongsGetComponent implements OnInit {
+
+  
+   songsData=[];
+
+  constructor(private _songsGetService : SongsGetService) { }
+
+  ngOnInit() {
+    this._songsGetService.getSongs().subscribe(songs=>{
+            console.log(songs);
+            this.songsData.push(songs);
+    })
+  }
+}
